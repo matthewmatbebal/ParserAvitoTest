925a2e3ea817fda75ca9f668f4626856baeba272
alexov <aleksov2020@gmail.com>
2024-03-08 13:27:44 +0400

    Add frontend

--- static/main.js
+++ static/main.js
@@ -1,45 +1,73 @@
 function getData() {
-    let link_psrsm = "test"
+    const spinner = document.getElementById('spinner');
+    spinner.classList.remove("hidden")
+
+
+    let inputValue = document.getElementById('input-link').value;
     let xhr = new XMLHttpRequest();
 
     xhr.open("POST", "http://localhost:5000/getData", true);
     xhr.setRequestHeader("Content-Type", "application/json");
 
-    let data = {
-        link: link_psrsm,
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === XMLHttpRequest.DONE) {
+        if (xhr.status === 200) {
+          let response = JSON.parse(xhr.responseText);
+          displayData(response)
+          spinner.classList.add("hidden")
+        } else {
+          console.error('Request failed: ' + xhr.status);
+        }
+      }
     };
 
-    xhr.send(JSON.stringify(data));
+    let data = {
+        link: inputValue,
+    };
+
+    xhr.send(JSON.stringify(data))
 }
 
 function displayData(data) {
-    const dataContainer = document.getElementById('dataContainer');
+    const dataContainer = document.getElementById('tbody');
     dataContainer.innerHTML = '';
-
-    const table = document.createElement('table');
-    const tbody = document.createElement('tbody');
 
     data.forEach(item => {
         const row = document.createElement('tr');
+
+        const idCell = document.createElement('th');
+        idCell.textContent = item.id;
+
         const linkCell = document.createElement('td');
-        linkCell.textContent = item.link;
-        const priceCell = document.createElement('td');
-        priceCell.textContent = item.price;
-        const nameCell = document.createElement('td');
-        nameCell.textContent = item.name;
+        linkCell.innerHTML = '<a href="' + item.link + '"> Link to AD </a>';
 
-        // Добавляем ячейки в строку
+        const squareField = document.createElement('td');
+        squareField.textContent = item.square_field;
+
+        const squareHouse = document.createElement('td');
+        squareHouse.textContent = item.square_house;
+
+        const price = document.createElement('td');
+        price.textContent = item.price;
+
+        const price_per_meter = document.createElement('td');
+        price_per_meter.textContent = item.price_per_meter;
+
+        const phone = document.createElement('td');
+        phone.textContent = item.phone;
+
+        const views_count = document.createElement('td');
+        views_count.textContent = item.views_count;
+
+        row.appendChild(idCell);
         row.appendChild(linkCell);
-        row.appendChild(priceCell);
-        row.appendChild(nameCell);
+        row.appendChild(squareField);
+        row.appendChild(squareHouse);
+        row.appendChild(price);
+        row.appendChild(price_per_meter);
+        row.appendChild(phone);
+        row.appendChild(views_count);
 
-        // Добавляем строку в тело таблицы
-        tbody.appendChild(row);
+        dataContainer.appendChild(row);
     });
-
-    // Добавляем тело таблицы в таблицу
-    table.appendChild(tbody);
-
-    // Добавляем таблицу в контейнер для данных
-    dataContainer.appendChild(table);
 }
